9b88c7828c4645c8b96a62085bd09cf41b677b0f
Kirtan Shah <shahkirtan2003@gmail.com>
2023-01-20 18:41:07 +0530

    Modified homepage

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,8 +1,8 @@
-import './style.css'
+import "./style.css";
 export const Headbar = () => {
   return (
     <div id="headb">
       <h1>Cryptolograpy in Indian Languages</h1>
     </div>
-  )
-}
+  );
+};
